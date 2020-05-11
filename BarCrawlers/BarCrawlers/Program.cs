283c2944f784de05dda518df195dcddf91f41c48
Ivan Lubomirov Ivanov <fizik_ivan@abv.bg>
2020-05-11 04:30:00 +0300

    Using DI for services injection into seeder class creating roles Magician and Crawler, creating MasterMagician(first magician user).

--- BarCrawlers/BarCrawlers/Program.cs
+++ BarCrawlers/BarCrawlers/Program.cs
@@ -2,8 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BarCrawlers.Data;
+using BarCrawlers.Data.DBModels;
+using BarCrawlers.Data.Seeder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +16,27 @@ namespace BarCrawlers
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    //var context = services.GetRequiredService<BCcontext>();
+                    var usermanager = services.GetRequiredService<UserManager<User>>();
+                    var rolemanager = services.GetRequiredService<RoleManager<Role>>();
+                    await BCSeeder.InitAsync(usermanager, rolemanager);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
