a9236d264187bf1c51c76d2d8b5076543bd946ea
grinikolov <gri.nikolov@gmail.com>
2020-05-16 11:09:07 +0300

    Added global model filters for IsDeleted.

---
+++ BarCrawlers/BarCrawlers.Tests/CocktailsServiceTests/Delete_Should.cs
@@ -0,0 +1,80 @@
+﻿using BarCrawlers.Data;
+using BarCrawlers.Data.DBModels;
+using BarCrawlers.Services;
+using BarCrawlers.Services.DTOs;
+using BarCrawlers.Services.Mappers;
+using BarCrawlers.Services.Mappers.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BarCrawlers.Tests.CocktailsServiceTests
+{
+    [TestClass]
+    public class Delete_Should
+    {
+        [TestMethod]
+        public void DeleteCocktail_ShouldReturnTrue_when_Valid()
+        {
+
+            //Arrange
+            var options = Utils.GetOptions(nameof(DeleteCocktail_ShouldReturnTrue_when_Valid));
+            // TODO: Create some ingredients
+           // var ingredient1 = new Ingredient { Id = Some Guid};
+
+            // save them to the database
+            var entity = new Cocktail
+            {
+
+
+
+              //  Ingredients = 
+            };
+
+
+            var mockMapper = new Mock<ICocktailMapper>();
+
+            using (var arrangeContext = new BCcontext(options))
+            {
+                arrangeContext.Cocktails.Add(entity);
+                arrangeContext.SaveChanges();
+            }
+
+            //Act & Assert
+            using (var context = new BCcontext(options))
+            {
+                var sut = new CocktailsService(context, mockMapper.Object);
+
+                var result = sut.DeleteAsync(Utils.MySampleGuid()).Result;
+
+                Assert.IsTrue(result);
+            }
+        }
+
+
+
+        [TestMethod]
+        public void DeleteCocktail_ReturnFalse_when_NotValidIngredientID()
+        {
+            //Arrange
+            var options = Utils.GetOptions(nameof(DeleteCocktail_ReturnFalse_when_NotValidIngredientID));
+            //var entity = new
+            var mockMapper = new Mock<ICocktailMapper>();
+
+            //Act & Assert
+            using (var context = new BCcontext(options))
+            {
+                var sut = new CocktailsService(context, mockMapper.Object);
+
+                var result = sut.DeleteAsync(Utils.MySampleGuid()).Result;
+
+                Assert.IsFalse(result);
+            }
+
+
+
+        }
+    }
+}
