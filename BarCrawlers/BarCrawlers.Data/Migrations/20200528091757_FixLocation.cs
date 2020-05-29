99e465d510d519d6c3d898c468fff72a2e9d0e28
Ivan Lubomirov Ivanov <fizik_ivan@abv.bg>
2020-05-29 14:13:57 +0300

    Migrations after change of Location structure

---
+++ BarCrawlers/BarCrawlers.Data/Migrations/20200528091757_FixLocation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BarCrawlers.Data.Migrations
+{
+    public partial class FixLocation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bars_Locations_LocationId",
+                table: "Bars");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "LocationId",
+                table: "Bars",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bars_Locations_LocationId",
+                table: "Bars",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bars_Locations_LocationId",
+                table: "Bars");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "LocationId",
+                table: "Bars",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bars_Locations_LocationId",
+                table: "Bars",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- BarCrawlers/BarCrawlers.Data/Migrations/20200528100244_MoreFixForLocation.Designer.cs
+++ BarCrawlers/BarCrawlers.Data/Migrations/20200528100244_MoreFixForLocation.Designer.cs
@@ -4,14 +4,16 @@ using BarCrawlers.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BarCrawlers.Data.Migrations
 {
     [DbContext(typeof(BCcontext))]
-    partial class BCcontextModelSnapshot : ModelSnapshot
+    [Migration("20200528100244_MoreFixForLocation")]
+    partial class MoreFixForLocation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
